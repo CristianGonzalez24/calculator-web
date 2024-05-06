e7046f012744c31c16de1d033dbbe959cd764769
CristianGonzalez24 <crissfcacho@gmail.com>
2024-05-06 11:37:52 -0300

    Add:
    Add class selectors in Javascript

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-
+const display = document.querySelector('.calculator__display');
+const buttons = document.querySelectorAll('.calculator__btn');
+const history = document.querySelector('node__history--list');
+const memory = document.querySelector('.node__memory--list');
 
