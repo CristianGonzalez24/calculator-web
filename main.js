9a888a0b75e820e5cc1b18505ed3198589c32816
CristianGonzalez24 <crissfcacho@gmail.com>
2024-05-07 11:40:37 -0300

    Add:
    Add variables and constants to handle calculations

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const display = document.querySelector('.calculator__display');
 const buttons = document.querySelectorAll('.calculator__btn');
-const history = document.querySelector('node__history--list');
-const memory = document.querySelector('.node__memory--list');
+const historyList = document.querySelector('node__history--list');
+const memoryList = document.querySelector('.node__memory--list');
+const specialChars = ['/', '*', '+', '-', '%', '=', '^', '!', '√'];
+const historyLength = 7;
+const memoryLimit = 10;
+const trigFunctionRegex = /(sin|cos|tan)(\d+(\.\d+)?)?/;
+let history = [];
+let memory = [];
+let output = '';
 
